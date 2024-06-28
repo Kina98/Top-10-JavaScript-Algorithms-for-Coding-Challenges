83b7f61c3c7a3dab053a7c94e302ae8a4cbf2414
Boubacar Kina <boubacarkina1@gmail.com>
2024-06-28 11:54:16 +0100

    reverse

---
+++ 1-reverse/reverseInt.js
@@ -0,0 +1,22 @@
+// on prend un nombre et on return son inverse
+
+function revInt(n) {
+    let intStr = n.toString();
+    let reversed = '';
+    for (const i of intStr) {
+        reversed = i + reversed;
+    }
+    return parseInt(reversed) * Math.sign(n);
+}
+
+console.log(revInt(4202));
+
+
+// autre maniere
+// function reverse(n) {
+//     const reversed = n.toString().split('').reverse().join('');
+
+//     return reversed;
+// }
+
+// console.log(reverse(4202));
