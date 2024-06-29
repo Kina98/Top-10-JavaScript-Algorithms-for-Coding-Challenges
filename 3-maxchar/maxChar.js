7758873dea371ded351aad9518aa11e116160116
Boubacar Kina <boubacarkina1@gmail.com>
2024-06-29 22:22:56 +0100

    maxchar

---
+++ 3-maxchar/maxChar.js
@@ -0,0 +1,10 @@
+// donner une chaine, et on return le caractére le plus fréquent dans cette chaine
+// Exemples: maxChar("codeloccol") === "c"
+
+function maxChar(str) {
+    const chaine = str.split('');
+    for (let i = 0; i < chaine.length; i++) {
+        
+        
+    }
+}
